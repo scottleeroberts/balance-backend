8d5ce215cceae7bed8a67d342b462858147f3474
Scott Roberts <scott.lee.roberts@gmail.com>
2024-03-23 07:14:52 -0400

    Configure JWT support

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_23_110851) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_23_111147) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -21,7 +21,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_23_110851) do
     t.datetime "updated_at", null: false
     t.string "full_name"
     t.string "username"
+    t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
