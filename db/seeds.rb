a730c8671fda24ab1868ce20c6e238164cbd01c1
Scott Roberts <scott.lee.roberts@gmail.com>
2024-03-23 16:20:25 -0400

    Improve Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,21 @@
 10.times do
+  User.create(
+    full_name: Faker::Name.name,
+    email: Faker::Internet.email,
+    username: Faker::Internet.username,
+    password: Faker::Internet.password,
+    jti: SecureRandom.uuid
+  )
+
   Game.create(
     name: Faker::Game.title,
     url: Faker::Internet.url,
     category: %w[math reading speaking writing].sample
   )
-end
 
+  GameEvent.create(
+    game_id: Game.all.sample.id,
+    occurred_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+    ge_type: %w[completed].sample
+  )
+end
