8aa3641e81ae9ea2ce99b58fa99809ac408369fd
Scott Roberts <scott.lee.roberts@gmail.com>
2024-03-23 19:10:41 -0400

    Clean up

--- app/controllers/api/game_events_controller.rb
+++ app/controllers/api/game_events_controller.rb
@@ -1,20 +1,6 @@
 class Api::GameEventsController < ApplicationController
   before_action :authenticate_user!
-  # before_action :set_game_event, only: %i[show update destroy]
 
-  # # GET /game_events
-  # def index
-  #   @game_events = GameEvent.all
-
-  #   render json: @game_events
-  # end
-
-  # # GET /game_events/1
-  # def show
-  #   render json: @game_event
-  # end
-
-  # POST /game_events
   def create
     @game_event = GameEvent.new(game_event_params.except(:type).merge(user: current_user))
 
@@ -31,28 +17,8 @@ class Api::GameEventsController < ApplicationController
     end
   end
 
-  #   # PATCH/PUT /game_events/1
-  #   def update
-  #     if @game_event.update(game_event_params)
-  #       render json: @game_event
-  #     else
-  #       render json: @game_event.errors, status: :unprocessable_entity
-  #     end
-  #   end
-
-  # DELETE /game_events/1
-  # def destroy
-  #   @game_event.destroy!
-  # end
-
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  # def set_game_event
-  #   @game_event = GameEvent.find(params[:id])
-  # end
-
-  # Only allow a list of trusted parameters through.
   def game_event_params
     params.require(:game_event).permit(:type, :occurred_at, :game_id)
   end
